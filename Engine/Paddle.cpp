5a427ed9e655f36a711c3afaa4f596872d35ee4b
authornoai <authornoai@gmail.com>
2019-03-06 16:30:07 +0300

    Paddle was added

---
+++ Engine/Paddle.cpp
@@ -0,0 +1,58 @@
+#include "Paddle.h"
+
+
+Paddle::Paddle(const Vec2 & in_pos, float in_halfWidth, float in_halfHeight)
+	:
+	pos(in_pos),
+	halfWidth(in_halfWidth),
+	halfHeigth(in_halfHeight)
+{
+}
+
+void Paddle::Draw(Graphics & graph) const
+{
+	graph.DrawRect(GetRect(), color);
+}
+
+bool Paddle::DoBallCollision(Ball & ball) const
+{
+	bool result = false;
+
+	if (GetRect().isOverlappingWith(ball.GetRect))
+	{
+		ball.ReboundY();
+		result = true;
+	}
+
+	return result;
+}
+
+bool Paddle::DoWallCollision(const RectF & walls)
+{
+	const RectF rect = GetRect();
+	if (rect.left < walls.left)
+	{
+		pos.x += walls.left - rect.left;
+	}
+	else if (rect.right > walls.right)
+	{
+		pos.x -= rect.right - walls.right;
+	}
+}
+
+void Paddle::Update(const Keyboard& kbd, float dt)
+{
+	if (kbd.KeyIsPressed(VK_LEFT))
+	{
+		pos.x -= speed * dt;
+	}
+	else if (kbd.KeyIsPressed(VK_RIGHT))
+	{
+		pos.x += speed * dt;
+	}
+}
+
+RectF Paddle::GetRect() const
+{
+	return RectF::FromCenter(pos, halfWidth, halfHeigth);
+}
--- Engine/Paddle.h
+++ Engine/Paddle.h
@@ -1,13 +1,25 @@
 #pragma once
 
 #include "Graphics.h"
+#include "Ball.h"
+#include "Keyboard.h"
 #include "Vec2.h"
 
 class Paddle
 {
+public:
+	Paddle(const Vec2& in_pos, float in_halfWidth, float in_halfHeight);
+
+	void Draw(Graphics& graph) const;
+	bool DoBallCollision(Ball& ball) const;
+	bool DoWallCollision(const RectF& walls);
+	void Update(const Keyboard& kbd, float dt);
+	RectF GetRect() const;
+
 private:
 	Color color = Colors::White;
-	const float halfWidht;
-	const float halfHeight;
+	const float halfWidth;
+	const float halfHeigth;
+	float speed;
 	Vec2 pos;
 };
